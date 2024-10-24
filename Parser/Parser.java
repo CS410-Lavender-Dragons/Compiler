f7d4d5d3c89b22de23338e69dc018316d5785ebd
skylerputney <121919826+skylerputney@users.noreply.github.com>
2024-10-24 09:56:57 -0400

    Parser setup
    -accept, expect functions

--- Parser/Parser.java
+++ Parser/Parser.java
@@ -1,6 +1,23 @@
 package Parser;
 
+import Core.Token;
+import Core.TokenName;
+
+import java.util.Queue;
+
 public class Parser {
+    Queue<Token> tokenQueue;
+    boolean accept(TokenName tokenName){
+        if (tokenQueue.peek().getName() == tokenName){
+            tokenQueue.remove();
+            return true;
+        }
+        return false;
+    }
 
-
+    void expect(TokenName tokenName){
+        if (tokenQueue.peek().getName() != tokenName)
+            throw new RuntimeException("Expected: " + tokenName + ", but found: " + tokenQueue.peek().getName());
+        tokenQueue.remove();
+    }
 }
