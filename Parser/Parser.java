a329f8a5d9b2bcab63c7b447892b4a10346b323f
skylerputney <121919826+skylerputney@users.noreply.github.com>
2024-10-28 14:03:36 -0400

    Parser functions

--- Parser/Parser.java
+++ Parser/Parser.java
@@ -20,4 +20,57 @@ public class Parser {
             throw new RuntimeException("Expected: " + tokenName + ", but found: " + tokenQueue.peek().getName());
         tokenQueue.remove();
     }
+
+    void STATEMENTS(){
+
+    }
+
+    void STATEMENT(){
+
+    }
+
+    void ASSIGNMENT_EXPR(){
+
+    }
+
+    void TYPE(){
+
+    }
+
+    void IF_EXPR(){
+
+    }
+
+    void WHILE_EXPR(){
+
+    }
+
+    void FOR_EXPR(){
+
+    }
+
+    void RANGE(){
+
+    }
+
+    void ARITHMETIC_EXPR(){
+
+    }
+
+    void TERM(){
+
+    }
+
+    void VALUE(){
+
+    }
+
+    void COMPARISON_EXPR(){
+
+    }
+
+    //Returns int representing complement operation of comparison token
+    int COMPARISON(){
+        return 0;
+    }
 }
